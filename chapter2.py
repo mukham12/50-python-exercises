0b449a86fbfe498ddd91170005d7ca1dffd9d16a
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-09-29 10:26:14 -0400

    Exercise 2.5 Sorting A String

--- chapter2.py
+++ chapter2.py
@@ -43,3 +43,15 @@ for letter in input("Enter a word: "):
 		print(f'ub{letter}', end='')
 	else:
 		print(letter, end='')
+
+'''
+Exercise 2.5 In this exercise, you’ll explore this idea by writing a function, strsort, that takes a single string 
+as its input, and returns a string. The returned string should contain the same characters as the input, except that 
+its characters should be sorted in order, from smallest Unicode value to highest Unicode value.
+
+For example, the result of invoking strsort('cba') will be the string abc.
+'''
+
+
+def strsort(s):
+	return ''.join(sorted(s))
