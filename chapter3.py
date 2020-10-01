bd29730b5acf228aec76862f32a210bb46f35220
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-10-01 08:55:48 -0400

    Implement plus_minus function and add decorations

--- chapter3.py
+++ chapter3.py
@@ -10,9 +10,11 @@ def firstlast(iterable):
 
 
 # A few test cases to check whether the function written above is working correctly.
-print(firstlast([1, 2, 3, 4]))  # Expected output: [1, 4]
-print(firstlast('abc'))  # Expected output: ac
-print(firstlast((3, 5, 1, 4)))  # Expected output: (3, 4)
+print(' ' * 14, 'Exercise 1', ' ' * 22, '*')
+print(firstlast([1, 2, 3, 4]), ' ' * 41, '*')  # Expected output: [1, 4]
+print(firstlast('abc'), ' ' * 45, '*')  # Expected output: ac
+print(firstlast((3, 5, 1, 4)), ' ' * 41, '*')  # Expected output: (3, 4)
+print('*' * 50)
 
 
 # Extension from the previous exercise
@@ -33,6 +35,34 @@ def even_odd_sum(iterable):
 
 
 # A few test cases to check whether the function written above is working correctly.
-print(even_odd_sum([10, 20, 30, 40, 50, 60]))  # Expected output: [90, 120]
-print(even_odd_sum((18, 6, 90, 35, 30, 36, 14)))  # Expected output: [152, 77]
-print(even_odd_sum([68, 70, 24, 49, 86, 26, 91, 38, 4]))  # Expected output: [273, 183]
+print(' ' * 14, 'Exercise 2', ' ' * 22, '*')
+print(even_odd_sum([10, 20, 30, 40, 50, 60]), ' ' * 38, '*')  # Expected output: [90, 120]
+print(even_odd_sum((18, 6, 90, 35, 30, 36, 14)), ' ' * 38, '*')  # Expected output: [152, 77]
+print(even_odd_sum([68, 70, 24, 49, 86, 26, 91, 38, 4]), ' ' * 37, '*')  # Expected output: [273, 183]
+print('*' * 50)
+
+
+# Extension exercise to the first exercise
+def plus_minus(iterable):
+	"""
+	Iterate over the array and add/subtract a number from a total depending on the
+	index the number is located in, namely if the number is located on an even index,
+	it has to be added to the total otherwise it has to be subtracted but first number is an exception.
+	:param iterable:
+	:return: int
+	"""
+	total = iterable[0]
+	for i, number in enumerate(iterable[1:]):
+		if i % 2 == 0:
+			total = total + number
+		else:
+			total = total - number
+	return total
+
+
+# A few test cases to check whether the function written above is working correctly.
+print(' ' * 14, 'Exercise 3')
+print(plus_minus([10, 20, 30, 40, 50, 60]))  # Expected output: 50
+print(plus_minus((8, 68, 46, 85, 16, 96, 58, 23)))  # Expected output: 160
+print(plus_minus([25, 49, 67, 85, 86]))  # Expected output: 6
+print('*' * 50)
