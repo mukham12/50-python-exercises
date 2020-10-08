c1c1d447f49065ff1a811661e16d2faa7689137a
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-10-08 08:21:26 -0400

    Format sorted name records

--- chapter3.py
+++ chapter3.py
@@ -182,3 +182,27 @@ print(most_repeating_word(['dividend', 'dialect', 'ensure', 'radio', 'announceme
       ' ' * 35, '*')
 print(most_repeating_word(['escape', 'reader', 'relaxation', 'perforate', 'government', 'consultation']), ' ' * 41, '*')
 print('*' * 50)
+
+PEOPLE = [('Donald', 'Trump', 7.85),
+          ('Vladimir', 'Putin', 3.626),
+          ('Jinping', 'Xi', 10.603)]
+
+
+def format_sort_records(list_of_tuples):
+	'''
+	Sort the names according to their last name and time of arrival
+	and print as a table
+	
+	:param list_of_tuples: list
+	:return: sorted list with names
+	'''''
+	output = []
+	template = '{1:10} {0:10} {2:5.2f}'
+	for person in sorted(list_of_tuples, key=operator.itemgetter(1, 0)):
+		output.append(template.format(*person))
+	return output
+
+
+print(' ' * 14, 'Exercise 8', ' ' * 22, '*')
+print('\n'.join(format_sort_records(PEOPLE)), ' ' * 20, '*')
+print('*' * 50)
