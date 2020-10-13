0cb84e650eff7904f84afb06903497410716b09b
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-10-13 10:45:25 -0400

    Different dictionary function

--- chapter4.py
+++ chapter4.py
@@ -53,3 +53,39 @@ def get_rainfall():
 
 
 get_rainfall()
+
+
+def dictdiff(a, b):
+	"""
+	Write a function that takes two dictionaries as arguments.
+	The function returns a new dict that expresses the difference between the two dicts. If there are no differences
+	between the dicts, the function returns an empty dict. For each key-value pair that differs, the return value of
+	the function will have a key-value pair in which the value is a list containing the values from the two different
+	dicts. If one of the dicts does not contain that key, it should contain None.
+
+	:param a: dict
+	:param b: dict
+	:return: dict
+	"""
+
+	output = {}
+	all_keys = a.keys() | b.keys()
+
+	for key in all_keys:
+		if a.get(key) != b.get(key):
+			output[key] = [a.get(key), b.get(key)]
+	return output
+
+
+# A few test cases to check if the function above is working correctly
+d1 = {'a': 1, 'b': 2, 'c': 3}
+d2 = {'a': 1, 'b': 2, 'c': 4}
+print(dictdiff(d1, d1))
+print(dictdiff(d1, d2))
+
+d3 = {'a': 1, 'b': 2, 'd': 3}
+d4 = {'a': 1, 'b': 2, 'c': 4}
+print(dictdiff(d3, d4))
+
+d5 = {'a': 1, 'b': 2, 'd': 4}
+print(dictdiff(d1, d5))
