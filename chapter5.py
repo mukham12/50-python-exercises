48f30869c3012e8f80c7b5b7ceae6b4f0358414d
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-10-28 12:57:57 -0400

    Chapter 5 working with JSON files

--- chapter5.py
+++ chapter5.py
@@ -1,3 +1,4 @@
+import glob
 from collections import defaultdict
 from io import StringIO
 import csv
@@ -137,4 +138,26 @@ def print_scores(directory):
 	:param directory: Name of the directory
 	:return: None
 	"""
+	scores = {}
+
+	for filename in glob.glob(f'{directory}/*.json'):
+		scores[filename] = {}
+
+		with open(filename) as infile:
+			for result in json.load(infile):
+				for subject, score in result.items():
+					scores[filename].setdefault(subject, [])
+					scores[filename][subject].append(score)
+
+	for one_class in scores:
+		print(one_class)
+		for subject, subject_scores in scores[one_class].items():
+			min_score = min(subject_scores)
+			max_score = max(subject_scores)
+			average_score = (sum(subject_scores), len(subject_scores))
+
+			print(subject)
+			print(f'\tmin {min_score}')
+			print(f'\tmax {max_score}')
+			print(f'\taverage {average_score}')
 
