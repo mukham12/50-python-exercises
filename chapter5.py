b963f62ebeb7c36b5d0e2cfb11ca955f660e417f
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-10-29 09:01:53 -0400

    Reverse the lines of a file

--- chapter5.py
+++ chapter5.py
@@ -161,3 +161,10 @@ def print_scores(directory):
 			print(f'\tmax {max_score}')
 			print(f'\taverage {average_score}')
 
+
+def reverse_lines(infilename, outfilename):
+	with open(infilename) as infile, open(outfilename, 'w') as outfile:
+		for one_line in infile:
+			outfile.write(f'{one_line.rstrip()[::-1]}\n')
+
+
