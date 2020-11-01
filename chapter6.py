1178389efc7c7532005a8a80b33aa3b2dc734c5b
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-11-01 11:42:37 -0500

    Random Password Generator

--- chapter6.py
+++ chapter6.py
@@ -1,4 +1,5 @@
 import operator
+import random
 
 
 def myxml(tagname, content='', **kwargs):
@@ -32,3 +33,29 @@ def calc(to_solve):
 
 
 print(calc('+ 2 3'))
+
+
+def create_password_generator(characters):
+	"""
+	Write a function that generates a random password from the given characters.
+	:param characters: a string of characters
+	:return: random password
+	"""
+
+	def create_password(length):
+		output = []
+		for _ in range(length):
+			output.append(random.choice(characters))
+		return ''.join(output)
+
+	return create_password
+
+
+alpha_password = create_password_generator('abcdef')
+symbol_password = create_password_generator('!@#$%')
+
+print(alpha_password(5))
+print(alpha_password(10))
+
+print(symbol_password(5))
+print(symbol_password(10))
