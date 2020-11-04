9e3af35944d555919180f0e0e407ed95724af352
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-11-04 14:12:51 -0500

    Exercise 29 Using a one liner

--- chapter7.py
+++ chapter7.py
@@ -3,3 +3,15 @@ def join_numbers(numbers):
 
 
 print(join_numbers(range(15)))
+
+
+def sum_numbers(numbers):
+	"""
+	Write a function that sums the digits of the given string which could potentially contain anything besides digits.
+	:param numbers:
+	:return:
+	"""
+	return sum(int(number) for number in numbers.split() if number.isdigit())
+
+
+print(sum_numbers('1 2 3 a b c 4'))
