2de369df6663ee9711245aad77d4c211e56bd434
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-11-11 15:24:47 -0500

    Gematria part II

--- chapter7.py
+++ chapter7.py
@@ -110,3 +110,14 @@ def gematria_dict():
 
 
 print(gematria_dict())
+
+GEMATRIA = gematria_dict()
+
+
+def gematria_for(word):
+	return sum(GEMATRIA.get(one_char, 0) for one_char in word)
+
+
+def gematria_equal_words(input_word):
+	our_score = gematria_for(input_word.lower())
+	return [one_word.strip() for one_word in open('/usr/share/dict/words') if gematria_for(one_word.lower()) == our_score]
