92735fd824fbd278648819627362841599ed715b
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-11-19 20:46:08 -0500

    Modify freedonia.py file

--- freedonia.py
+++ freedonia.py
@@ -1,16 +1,17 @@
-from decimal import Decimal
-
-rates = {
-	'Chico': Decimal('0.5'),
-	'Groucho': Decimal('0.7'),
-	'Harpo': Decimal('0.5'),
-	'Zeppo': Decimal('0.4')
-	}
+RATES = {
+	'Chico': 0.5,
+	'Groucho': 0.7,
+	'Harpo': 0.5,
+	'Zeppo': 0.4
+}
 
 
 def time_percentage(hour):
-	return hour / Decimal('24.0')
+	return hour / 24
 
 
 def calculate_tax(amount, state, hour):
-	return float(amount + (amount * rates[state] * time_percentage(hour)))
+	return amount + (amount * RATES[state] * time_percentage(hour))
+
+
+print(calculate_tax(500, 'Harpo', 12))
