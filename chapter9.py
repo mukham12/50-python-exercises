20a23fe8b1b4608e57bc1fc52da89dcd28c8513c
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-12-03 10:50:14 -0500

    Chapter 9 ScoreList object

--- chapter9.py
+++ chapter9.py
@@ -2,3 +2,15 @@
 Chapter 9
 A chapter to learn about objects and object oriented programming.
 """
+
+
+class ScoreList:
+	def __init__(self, scores):
+		self.scores = scores
+
+	def average(self):
+		return sum(self.scores) / len(self.scores)
+
+
+scores = ScoreList([85, 95, 98, 87, 80, 92])
+print(f'The final score is {scores.average()}.')
