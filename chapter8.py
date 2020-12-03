f6d7a2d02f7b08041bbdd71eca3f435ff1020d07
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-12-03 10:38:20 -0500

    Chapter 8 functions

--- chapter8.py
+++ chapter8.py
@@ -5,3 +5,22 @@ tax_at_9pm = calculate_tax(100, 'Harpo', 21)
 
 print(f'You owe a total of: {tax_at_12noon}')
 print(f'You owe a total of: {tax_at_9pm}')
+
+
+def menu(**options):
+	while True:
+		option_string = ' / '.join(sorted(options))
+		choice = input(f'Enter an option ({option_string}): ')
+		return options[choice]() if choice in options else 'Not a valid option'
+
+
+def func_a():
+	return 'A'
+
+
+def func_b():
+	return 'B'
+
+
+return_value = menu(a=func_a, b=func_b)
+print(f'Result is {return_value}')
