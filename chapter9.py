b0c0b6fc0ac7c016f06e328c3b7a694c7f9b4609
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-12-11 10:05:32 -0500

    Modify Scoop and Bowl class to limit number of scoops

--- chapter9.py
+++ chapter9.py
@@ -42,12 +42,15 @@ Create a Bowl class representing a bowl into which we can put our ice cream. No 
 
 
 class Bowl:
+	max_scoops = 3
+
 	def __init__(self):
 		self.scoops = []
 
 	def add_scoops(self, *new_scoops):
 		for scoop in new_scoops:
-			self.scoops.append(scoop)
+			if len(self.scoops) < Bowl.max_scoops:
+				self.scoops.append(scoop)
 
 	def __repr__(self):
 		return '\n'.join(s.flavor.title() for s in self.scoops)
@@ -56,10 +59,13 @@ class Bowl:
 s1 = Scoop('chocolate')
 s2 = Scoop('vanilla')
 s3 = Scoop('persimmon')
+s4 = Scoop('flavor 4')
+s5 = Scoop('flavor 5')
 
 b = Bowl()
 b.add_scoops(s1, s2)
 b.add_scoops(s3)
+b.add_scoops(s4, s5)
 print(b)
 
 '''
