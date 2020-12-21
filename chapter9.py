47a2ac76869fcf9212468dca26edcf2837fa8495
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-12-21 08:20:33 -0500

    RyeBread class implementation

--- chapter9.py
+++ chapter9.py
@@ -252,3 +252,13 @@ class WholeWheatBread(Bread):
 		self.sodium = 138
 		self.sugar = 1.4
 		self.fat = 1
+
+
+class RyeBread(Bread):
+	def __init__(self):
+		super().__init__()
+		self.calories = 67
+		self.carbs = 12
+		self.sodium = 172
+		self.sugar = 1
+		self.fat = 0.8
