bf7c97468d9acfdd1d844e2ca7095e6162c06170
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-01-06 08:55:42 -0500

    Enumerate helper

--- chapter10.py
+++ chapter10.py
@@ -1,15 +1,21 @@
-class MyEnumerate:
-	"""Simple replacement for enumerate"""
-
+class MyEnumerateIterator:
 	def __init__(self, data):
 		self.data = data
 		self.index = 0
 
-	def __iter__(self):
-		return self
-
-	def __next__(self) -> tuple:
+	def __next__(self):
 		if self.index >= len(self.data):
 			raise StopIteration
+		value = (self.index, self.data[self.index])
 		self.index += 1
-		return self.index, self.data[self.index]
+		return value
+
+
+class MyEnumerate():
+	"""Simple replacement for enumerate"""
+
+	def __init__(self, data):
+		self.data = data
+
+	def __iter__(self):
+		return MyEnumerateIterator(self.data)
