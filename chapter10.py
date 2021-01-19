aa52030ea661506f1f65db3ac3173f884edfa798
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-01-19 12:19:18 -0500

    Elapsed since function overload

--- chapter10.py
+++ chapter10.py
@@ -162,3 +162,16 @@ def elapsed_since(data):
 		delta = current_time - (last_time or current_time)
 		last_time = time.perf_counter()
 		yield delta, item
+
+
+def elapsed_since(data, min_wait):
+	last_time = None
+	for item in data:
+		current_time = time.perf_counter()
+		delta = current_time - (last_time or current_time)
+
+		if delta < min_wait:
+			time.sleep(min_wait - delta)
+
+		last_time = time.perf_counter()
+		yield delta, item
