38e4464ee271b8ed19e64b4ffa0160df781ac0db
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-01-26 11:08:32 -0500

    Test is_iterable

--- chapter10.py
+++ chapter10.py
@@ -228,3 +228,8 @@ def myrange(first, second=None, step=1):
 def test_simple():
 	m = MyEnumerate('abc')
 	assert list(m) == [(0, 'a'), (1, 'b'), (2, 'c')]
+
+
+def test_is_iterable():
+	m = MyEnumerate('abc')
+	assert hasattr(m, '__iter__')
