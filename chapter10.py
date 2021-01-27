69167b532162076f5c060aee33d5406db85629ed
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-01-27 13:20:35 -0500

    Test circle

--- chapter10.py
+++ chapter10.py
@@ -1,5 +1,6 @@
 import os
 import time
+import pytest
 
 
 class MyEnumerateIterator:
@@ -233,3 +234,12 @@ def test_simple():
 def test_is_iterable():
 	m = MyEnumerate('abc')
 	assert hasattr(m, '__iter__')
+
+
+@pytest.mark.parametrize('iterable, maxtimes, output', [
+	('abcd', 7, 'abcdabc'),
+	([10, 20, 30], 2, [10, 20]),
+	([10, 20, 30], 8, [10, 20, 30, 10, 20, 30, 10, 20]),
+	])
+def test_circle(iterable, maxtimes, output):
+	assert list(Circle(iterable, maxtimes)) == list(output)
